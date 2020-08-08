0bda8755ec1b5a6f42f9771c4e419fa4a642f6c4
mayhashah7 <69404742+mayhashah7@users.noreply.github.com>
2020-08-08 19:01:58 -0400

    Add files via upload

---
+++ removeRGandResourceLocks.ps1
@@ -0,0 +1,11 @@
+﻿$lockedResourceGroups = Search-AzGraph -Query "resourcecontainers | where type == 'microsoft.resources/subscriptions/resourcegroups' | where tags.Lock=~'True'"
+
+foreach ($rg in $lockedResourceGroups) {
+    Remove-AzResourceLock -LockName "ExampleLock" -ResourceGroupName $rg.resourceGroup
+}
+
+$lockedResources = Search-AzGraph -Query "Resources | where tags.Lock=~'True'"
+
+foreach ($resource in $lockedResources) {
+    Remove-AzResourceLock -LockName "ExampleLock" -ResourceName $resource.name -ResourceType $resource.type -ResourceGroupName $resource.resourceGroup
+}
